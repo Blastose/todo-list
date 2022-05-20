e66d82a40b8a27b0c53bdcb14a3f33581225960c
Blastose <blastoisejay@gmail.com>
2022-05-20 10:59:21 -0700

    Setup Jest and ts-jest

---
+++ tests/unit/todo-models.test.ts
@@ -0,0 +1,6 @@
+import * as TodoModels from '../../src/todo-models';
+
+test('Create Todo Item', () => {
+  const todoItem = new TodoModels.TodoItem('Clean dishes', 'Clean the dishes with soup', new Date(), TodoModels.Priority.high, false);
+  expect(todoItem.title).toBe('Clean dishes');
+});
