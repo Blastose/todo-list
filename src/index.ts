675494b1d6c935e503e60b1213885b3baaffb987
Blastose <blastoisejay@gmail.com>
2022-05-25 16:05:25 -0700

    Expand TodoItem card on hover

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { v4 as uuidv4 } from 'uuid'
 const list = new TodoModels.TodoList();
 for (let i = 0; i < 10; i++) {
   const item = new TodoModels.TodoItem(`${i}`, 'This is my todo item', new Date(), TodoModels.Priority.none, false, uuidv4(), 'Old Project');
-  const item2 = new TodoModels.TodoItem(`${i}-${i}`, 'MY todo item', new Date(), TodoModels.Priority.medium, false, uuidv4(), 'My Project');
+  const item2 = new TodoModels.TodoItem(`${i}-${i}`, 'MY todo item', new Date(), TodoModels.Priority.low, false, uuidv4(), 'My Project');
   list.add(item);
   list.add(item2);
 }
